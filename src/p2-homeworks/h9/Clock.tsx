e254bb5dba3b6d33ada91ae0b3730ebadebf6668
AlexndrG <alexndr1@mail.ru>
2021-07-19 16:57:31 +0300

    simple react homeworks - дз9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,31 +1,47 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
+const getText = (n: number): string => {
+    return n < 10 ? '0' + n : '' + n
+}
+
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
         // stop
+        window.clearTimeout(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
             // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
         // show
+        setShow(true)
     }
     const onMouseLeave = () => {
         // close
+        setShow(false)
     }
+    // 'Time' // fix with date
+    const stringTime =
+        `${getText(date?.getHours())}:` +
+        `${getText(date?.getMinutes())}:` +
+        `${getText(date?.getSeconds())}`
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    // 'Date' // fix with date
+    const stringDate =
+        `${getText(date?.getDate())}-` +
+        `${getText(date?.getMonth() + 1)}-` +
+        `${date?.getFullYear()}`
 
     return (
         <div>
--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Clock from './Clock'
+import s from '../h9/HW9.module.css';
 
 function HW9() {
     return (
@@ -8,7 +9,9 @@ function HW9() {
             homeworks 9
 
             {/*should work (должно работать)*/}
-            <Clock/>
+            <div className={s.main}>
+                <Clock/>
+            </div>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
